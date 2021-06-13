1fb6360f4564ea70c4afd7cf1cbc2b89b3be0e55
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-06-14 01:59:05 +0530

    [WIP] proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { PDFDocument, degrees } from 'pdf-lib'
+
+const makePdf = async () => {
+  const pdfDoc = await PDFDocument.create()
+  const page = pdfDoc.addPage()
+
+  const sourcePdfUrl = 'https://pdf-lib.js.org/assets/with_large_page_count.pdf'
+  const sourcePdf = await fetch(sourcePdfUrl).then((res) => res.arrayBuffer())
+
+  const [embeddedPage] = await pdfDoc.embedPdf(sourcePdf, [73])
+  page.drawPage(embeddedPage, {
+    x: 250,
+    y: 200,
+    xScale: 0.5,
+    yScale: 0.5,
+    rotate: degrees(30),
+    opacity: 0.75,
+  })
+  
+  const pdfBytes = await pdfDoc.save()
+  var blob = new Blob([pdfBytes], { type: "application/pdf" });// change resultByte to bytes
+
+  var link = document.createElement('a');
+  link.href = window.URL.createObjectURL(blob);
+  link.download = "myFileName.pdf";
+  link.click();
+
+}
 
 function App() {
   return (
@@ -18,6 +45,11 @@ function App() {
         >
           Learn React
         </a>
+        <br /><br />
+        <button
+
+          onClick={() => makePdf()}
+        >Download Pdf</button>
       </header>
     </div>
   );
