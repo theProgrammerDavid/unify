e2c356dcab4055c9282515f4039e0285fc1ece8e
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-06-14 23:26:23 +0530

    feat: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import './index.css'
 import { PDFDocument, degrees } from 'pdf-lib'
 
 const makePdf = async () => {
@@ -18,9 +18,9 @@ const makePdf = async () => {
     rotate: degrees(30),
     opacity: 0.75,
   })
-  
+
   const pdfBytes = await pdfDoc.save()
-  var blob = new Blob([pdfBytes], { type: "application/pdf" });// change resultByte to bytes
+  var blob = new Blob([pdfBytes], { type: "application/pdf" });
 
   var link = document.createElement('a');
   link.href = window.URL.createObjectURL(blob);
@@ -31,26 +31,53 @@ const makePdf = async () => {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br /><br />
-        <button
+    <div className="uk-container uk-background-muted">
+      <p className="uk-padding-large uk-light uk-heading-large black grid">
+        Modern PDF Merger
+      </p>
+      <div className="uk-child-width-expand@s uk-text-center" uk-grid="true" >
+        <div>
+          <div>
+            <div className="uk-light uk-background-secondary uk-padding">
+              <h3>Fast af</h3>
+              <p>Native Asynchronous Javascript PDF Manipulation</p>
+              <button className="uk-button uk-button-default">Button</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className="uk-light uk-background-secondary uk-padding">
+              <h3>Privacy 💯</h3>
+              <p>All conversion done in your web prowser. No PDFs reach a server</p>
+              <button className="uk-button uk-button-default">Button</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className="uk-light uk-background-secondary uk-padding">
+              <h3>Versatile </h3>
+              <p>Coming soon - Doc and PPT Manipulation</p>
+              <button className="uk-button uk-button-default">Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
-          onClick={() => makePdf()}
-        >Download Pdf</button>
-      </header>
+      <br /><br /><br />
+
+      <div className="js-upload uk-placeholder uk-text-center grid ">
+        <span uk-icon="icon: cloud-upload"></span>
+        <span className="uk-text-middle">Attach binaries by dropping them here or</span>
+        <div uk-form-custom>
+          <input type="file" multiple />
+        </div>
+      </div>
+
+      <progress id="js-progressbar" className="uk-progress" value="0" max="100" hidden></progress>
+
+
     </div>
   );
 }
