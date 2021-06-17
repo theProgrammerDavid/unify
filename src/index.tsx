b9e140e4b207551d59f6932f597ce7c5dca7ea45
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-06-18 00:20:03 +0530

    refactor: unused web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,4 +11,3 @@ ReactDOM.render(
 );
 
 serviceWorkerRegistration.register();
-reportWebVitals(console.log);
